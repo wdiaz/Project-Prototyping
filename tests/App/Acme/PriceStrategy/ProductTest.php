5b52f0df1ef170bf3ae57a8c579f3b5527b04d48
Walter Diaz <wdiaz503@gmail.com>
2017-12-03 00:50:09 +0000

    Added A very simplistic Test (rush time ahead)

---
+++ tests/App/Acme/PriceStrategy/ProductTest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use App\Acme\PriceStrategy\Item;
+use App\Acme\PriceStrategy\Order;
+use App\Acme\PriceStrategy\Pricing\StandardPricing;
+use App\Acme\PriceStrategy\Product;
+use PHPUnit\Framework\TestCase;
+
+
+final class ProductTest extends TestCase
+{
+    public function testProduct()
+    {
+        $order = new Order();
+        $order->add(new Item(new Product('Pants', 25.25, 20.25), 4, new StandardPricing(true)));
+        $total = $order->getTotal();
+        $this->assertEquals(24.30, $total);
+    }
+
+
+    /*public function testWithProductPriceOverride()
+    {
+        $order = new Order();
+        $order->add(new Item(new Product('Pants', 25.25, 20.25), 4, new StandardPricing()));
+        $total = $order->getTotal();
+        $this->assertEquals(24.30, $total);
+    }*/
+}
